1e1e4dc75d81acbd44ed57a23c896fe1d74dad85
ps-mkraines <meir.kraines@crisphealth.org>
2023-12-27 15:36:32 -0500

    added green status with /status

--- Program.cs
+++ Program.cs
@@ -1,14 +1,17 @@
+
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddSignalR().AddAzureSignalR();
 
 var app = builder.Build();
 
 app.UseDefaultFiles();
 app.UseRouting();
+
 app.UseStaticFiles();
 
 app.MapHub<ChatHub>("/chat");
 
-//app.MapGet("/", () => "Hello World!");
+app.MapGet("/status", () => "Status: Green.");
 
 app.Run();
